e01f9492957dddd67c6fdaa4dda835bf70e24ab2
GregoryShute <gregory.shute.5@gmail.com>
2016-06-16 19:47:50 -0400

    Update FriendshipBehavior.js

--- Model/Behaviors/FriendshipBehavior.js
+++ Model/Behaviors/FriendshipBehavior.js
@@ -1,4 +1,4 @@
-var knex = require('../database/knex');
+var knex = require('../Database/knex');
 var Promise = require('bluebird');
 var FriendshipBehaviorMotor = require('../BehaviorMotors/FriendshipBehaviorMotor');
 var LookupBehavior = require('./LookupBehavior');
